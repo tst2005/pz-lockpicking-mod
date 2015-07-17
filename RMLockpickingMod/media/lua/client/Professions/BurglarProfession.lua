71035e229bf4a3eb7b522e57a4fa05926cf9dfd1
Robert Machmer <robert.machmer@gmail.com>
2015-07-17 15:17:09 +0200

    Whitespace clean up

--- RMLockpickingMod/media/lua/client/Professions/BurglarProfession.lua
+++ RMLockpickingMod/media/lua/client/Professions/BurglarProfession.lua
@@ -40,22 +40,21 @@ end
 -- Create custom professions.
 --
 local function initProfessions()
-    --Was originally just modifying existing profession to inject traits, however reinitializing and overwriting
-    --the whole profession was the only way I could find to get the icon to actually change.
-    --Wouldn't have been an issue if setIconPath also did the Java logic to reload the texture, but it doesn't so
-    --this is the ugly workaround for that. Should still be update proof should the original Burlar change.
-
+    -- Was originally just modifying existing profession to inject traits, however reinitializing and overwriting
+    -- the whole profession was the only way I could find to get the icon to actually change.
+    -- Wouldn't have been an issue if setIconPath also did the Java logic to reload the texture, but it doesn't so
+    -- this is the ugly workaround for that. Should still be update proof should the original Burlar change.
     local origBurgler = ProfessionFactory.getProfession(BURGLAR_ID);
 
-        local burglar = ProfessionFactory.addProfession(BURGLAR_ID, getText("UI_prof_Burglar"), "Prof_nk_Burglar", origBurgler:getCost()-2);
-        for k,v in pairs(transformIntoKahluaTable(origBurgler:getXPBoostMap())) do
-            burglar:addXPBoost(k, v);
-        end
-        burglar:setFreeTraitStack(origBurgler:getFreeTraitStack());
-        burglar:addFreeTrait("nimblefingers");
-        burglar:addFreeTrait("NightOwl");
+    local burglar = ProfessionFactory.addProfession(BURGLAR_ID, getText("UI_prof_Burglar"), "Prof_nk_Burglar", origBurgler:getCost()-2);
+    for k, v in pairs(transformIntoKahluaTable(origBurgler:getXPBoostMap())) do
+        burglar:addXPBoost(k, v);
+    end
+    burglar:setFreeTraitStack(origBurgler:getFreeTraitStack());
+    burglar:addFreeTrait("nimblefingers");
+    burglar:addFreeTrait("NightOwl");
 
-        burglar:setDescription(origBurgler:getDescription().." <LINE> "..getText("UI_trait_nimblefingers").." <LINE> "..getText("UI_trait_nightowl"));
+    burglar:setDescription(origBurgler:getDescription() .. " <LINE> " .. getText("UI_trait_nimblefingers") .. " <LINE> " .. getText("UI_trait_nightowl"));
 end
 
 ---
